6b770acab6572dcbaf1e391faa06e34aa75ea597
Sangwon Park <18sp01@gmail.com>
2021-11-21 17:42:22 +0800

    seperated the files into picture, text, and merge

---
+++ components/GameText.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+import picture_name from "../assets/data/picture_list.json";
+
+const PictureGame = () => {
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      },
+ });
