52ec0ca9055921b971f3dde36b457b3297ad4fce
abheeg06 <u3560162@connect.hku.hk>
2021-11-22 16:26:18 +0530

    padding added

--- components/GamePicture.js
+++ components/GamePicture.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import all from '../assets/data/picture_list.json';
+
 import {
   StyleSheet,
   Text,
@@ -8,16 +10,48 @@ import {
   Button,
   Image,
   FlatList, 
+  Alert,
 } from "react-native";
 
-const onPress = () =>(1);
+
+
+function randomArrayShuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+
+const wordList = require('../assets/data/picture_list.json');
+
+console.log(wordList["all"])
+
+const wrongAns = () =>
+Alert.alert(
+  "Wrong Answer",
+  "The Answer is wrong",
+  [
+    {
+      text: "Cancel",
+      onPress: () => console.log("Cancel Pressed"),
+      style: "cancel"
+    },
+    { text: "OK", onPress: () => console.log("OK Pressed") }
+  ]
+);
 
 const GamePicture = () => {
   const [images, setimages]= useState([
-    require('../assets/pictures/animals/bear.jpg'),
-    require('../assets/pictures/animals/bee.jpg'),
-    require('../assets/pictures/animals/cow.jpg'),
-    require('../assets/pictures/animals/crow.jpg'),
+    require('../assets/pictures/all/bear.jpg'),
+    require('../assets/pictures/all/bee.jpg'),
+    require('../assets/pictures/all/cow.jpg'),
+    require('../assets/pictures/all/crow.jpg'),
   ]);
 
   return (
@@ -26,7 +60,7 @@ const GamePicture = () => {
         numColumns={2}
         data={images}
         renderItem={ ({item, index }) => (
-          <TouchableOpacity onPress={onPress}>
+          <TouchableOpacity onPress={(wrongAns)}>
           <Image source = {item}
           key={index}
           style={styles.tinyLogo}
@@ -37,6 +71,9 @@ const GamePicture = () => {
     </View>
   );
 };
+
+
+
 
 const styles = StyleSheet.create({
   tinyLogo:{
